eda86a15b78bce752f4279e141636faa729e039f
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-11-09 02:23:41 -0200

    create basic model post

---
+++ bingo/post/migrations/0001_initial.py
@@ -0,0 +1,35 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Post'
+        db.create_table('post_post', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=150)),
+            ('content', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal('post', ['Post'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'Post'
+        db.delete_table('post_post')
+
+
+    models = {
+        'post.post': {
+            'Meta': {'object_name': 'Post'},
+            'content': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '150'})
+        }
+    }
+
+    complete_apps = ['post']
--- bingo/post/models.py
+++ bingo/post/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+
+    def __unicode__(self):
+        return self.title
