f5a2d5124e97a7a14d15d186835ae2c6b7116410
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-11-09 02:25:48 -0200

    index return list of posts

--- bingo/urls.py
+++ bingo/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 from django.contrib import admin
+
+from post.models import Post
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name="index.html"), name='home'),
+    url(r'^$', ListView.as_view(model=Post,
+                                template_name="index.html"
+                               ),
+        name='home'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
