7d0a747d61f0f380bf8acae85c2b780bedbfc93a
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-11-09 00:12:54 -0200

    admin urls

--- bingo/urls.py
+++ bingo/urls.py
@@ -5,12 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^$', direct_to_template, {"template": "index.html"}, name='home'),
-    # url(r'^bingo/', include('bingo.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', direct_to_template, {"template": "index.html"}, name='home'),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
