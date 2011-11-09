fa99a05163d40366347853da5566fc505dce77fa
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-11-09 00:55:14 -0200

    class based views

--- bingo/urls.py
+++ bingo/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', direct_to_template, {"template": "index.html"}, name='home'),
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name='home'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
