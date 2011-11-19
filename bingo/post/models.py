c67ece45d3b8fed5787aad304a10f03341d317d5
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-11-19 04:02:16 -0200

    permalink decorator

--- bingo/post/models.py
+++ bingo/post/models.py
@@ -7,5 +7,6 @@ class Post(models.Model):
     def __unicode__(self):
         return self.title
 
+    @models.permalink
     def get_absolute_url(self):
-        return "/posts/%s/" % self.id
+        return ("post_detail", [self.id])
