6845c5326a6c520c52517c5895ec17e4961a8bfb
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-11-19 03:58:26 -0200

    add url for post details

--- bingo/urls.py
+++ bingo/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.contrib import admin
 
 from post.models import Post
@@ -12,6 +12,12 @@ urlpatterns = patterns('',
                                 template_name="index.html"
                                ),
         name='home'),
+
+    url(r"^posts/(?P<pk>\w+)/$", DetailView.as_view(model=Post,
+                                                    template_name="post/post.html",
+                                                    context_object_name="post",
+                                                    ),
+       name="post_detail"),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
